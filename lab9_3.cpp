b33345bcfddcd41f001c85f24d8ce80d2008e811
Chayathon <chayathon_p@cmu.ac.th>
2022-12-23 15:33:12 +0700

    lab9_3 COMPLETE

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -11,3 +11,53 @@
 "Brook"
 "Jinbe"
 */
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{ 
+     int y,h,b;
+     cout << "Enter your age: ";
+     cin >> y;
+     string character;
+     if(y <= 25)
+     {
+        cout << "Enter your height: ";
+        cin >> h;
+        if(h<100)
+        
+        character = "Chopper";
+        else if (h<180)
+        character = "Usopp";
+        else
+        {
+        cout << "Enter your bounty: ";    
+        cin >> b;
+        if(b <= 1100000000)
+        character = "Sanji";
+        else
+        character = "Zoro";
+        }
+     }
+     else if (y <= 60)
+     {
+        cout << "Enter your bounty: ";
+        cin >> b;
+        if(b>500000000)
+        character = "Jinbe";
+        else
+        character = "Franky";
+     }
+     else
+     character = "Brook";
+
+     cout << "Your character = " << character;
+     return 0;
+     }
+    
+    
+    
+    
+
+    
+
